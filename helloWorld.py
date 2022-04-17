9da913c8241a9f0ae39abc9d24fb275dc9af0058
M Fajri Al Banna <fajri060801@gmail.com>
2022-04-18 00:56:40 +0700

    fourth commit

--- helloWorld.py
+++ helloWorld.py
@@ -6,3 +6,10 @@ if (n % 2) == 0:
     print (n,"\tgenap")
 else:
     print (n,"\tganjil")
+
+num1, num2 = 0, 1
+for i in range(0, n):
+    print (num2)
+    num = num1 + num2
+    num1=num2
+    num2 = num
